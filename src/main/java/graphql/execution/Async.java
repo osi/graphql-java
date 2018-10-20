340fea1371e76febd6c0d8e4cdce742a346ff79e
peter royal <proyal@netflix.com>
2018-10-20 14:39:17 -0700

    remove Async methods only used in tests

--- src/main/java/graphql/execution/Async.java
+++ src/main/java/graphql/execution/Async.java
@@ -1,19 +1,12 @@
 package graphql.execution;
 
-import graphql.Assert;
 import graphql.Internal;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 @Internal
 @SuppressWarnings("FutureReturnValueIgnored")
@@ -38,43 +31,8 @@ public class Async {
         return overallResult;
     }
 
-    public static <T, U> Flux<U> each(Iterable<T> list, BiFunction<Long, T, Mono<U>> cfFactory) {
-        return Flux.fromIterable(list)
-                   .index(cfFactory)
-                   .flatMapSequential(Function.identity());
-    }
-
-    public static <T, U> CompletableFuture<List<U>> eachSequentially(Iterable<T> list, CFFactory<T, U> cfFactory) {
-        CompletableFuture<List<U>> result = new CompletableFuture<>();
-        eachSequentiallyImpl(list.iterator(), cfFactory, 0, new ArrayList<>(), result);
-        return result;
-    }
-
-    private static <T, U> void eachSequentiallyImpl(Iterator<T> iterator, CFFactory<T, U> cfFactory, int index, List<U> tmpResult, CompletableFuture<List<U>> overallResult) {
-        if (!iterator.hasNext()) {
-            overallResult.complete(tmpResult);
-            return;
-        }
-        CompletableFuture<U> cf;
-        try {
-            cf = cfFactory.apply(iterator.next(), index, tmpResult);
-            Assert.assertNotNull(cf, "cfFactory must return a non null value");
-        } catch (Exception e) {
-            cf = new CompletableFuture<>();
-            cf.completeExceptionally(new CompletionException(e));
-        }
-        cf.whenComplete((cfResult, exception) -> {
-            if (exception != null) {
-                overallResult.completeExceptionally(exception);
-                return;
-            }
-            tmpResult.add(cfResult);
-            eachSequentiallyImpl(iterator, cfFactory, index + 1, tmpResult, overallResult);
-        });
-    }
-
     /**
-     * Turns an object T into a CompletableFuture if its not already
+     * Turns an object T into a Mono if its not already
      *
      * @param t   - the object to check
      * @param <T> for two
@@ -92,37 +50,6 @@ public class Async {
         } else {
             return Mono.justOrEmpty(t);
         }
-    }
-
-    public static <T> CompletableFuture<T> tryCatch(Supplier<CompletableFuture<T>> supplier) {
-        try {
-            return supplier.get();
-        } catch (Exception e) {
-            CompletableFuture<T> result = new CompletableFuture<>();
-            result.completeExceptionally(e);
-            return result;
-        }
-    }
-
-    public static <T> CompletableFuture<T> exceptionallyCompletedFuture(Throwable exception) {
-        CompletableFuture<T> result = new CompletableFuture<>();
-        result.completeExceptionally(exception);
-        return result;
-    }
-
-    public static <T> void copyResults(CompletableFuture<T> source, CompletableFuture<T> target) {
-        source.whenComplete((o, throwable) -> {
-            if (throwable != null) {
-                target.completeExceptionally(throwable);
-                return;
-            }
-            target.complete(o);
-        });
-    }
-
-    @FunctionalInterface
-    public interface CFFactory<T, U> {
-        CompletableFuture<U> apply(T input, int index, List<U> previousResults);
     }
 
 }
